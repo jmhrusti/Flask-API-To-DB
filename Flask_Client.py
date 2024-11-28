c4c3b44d439f7cc79d69d06bdb98108283a266b9
jmhrusti <145395037+jmhrusti@users.noreply.github.com>
2024-11-28 15:53:05 -0500

    Update Flask_Client.py
    Added Licensing.

--- Flask_Client.py
+++ Flask_Client.py
@@ -6,6 +6,22 @@ This python script serves to allow a user to push data in JSON form to the corre
 ** Note: This application does utilize unencrypted HTTP to push data. Avoid sending sensitive data using this application, including username/passwords, keys, PII, etc.
 * Note: Ensure all below libraries are installed with pip.
 
+
+   Copyright 2024 jmhrusti
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
 '''
 import requests
 import json
